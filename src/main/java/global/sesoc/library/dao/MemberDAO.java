bf046adca7c81030df6d6dd352737a65cae1d855
KIM SOHWI <sohwe05@gmail.com>
2022-02-15 09:34:29 +0900

    회원 가입 membercontroller, memberdao, membermapper

--- src/main/java/global/sesoc/library/dao/MemberDAO.java
+++ src/main/java/global/sesoc/library/dao/MemberDAO.java
@@ -4,12 +4,28 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import global.sesoc.library.mapper.MemberMapper;
+import global.sesoc.library.vo.Members;
+
 
 
 @Repository
 public class MemberDAO {
 	@Autowired
 	SqlSession sqlSession;
+
+	public int insert(Members member) {
+		MemberMapper mapper = sqlSession.getMapper(MemberMapper.class);
+		int result = 0;
+
+		try {
+			result = mapper.insertMember(member);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
 	
 
 }
--- src/main/java/global/sesoc/library/mapper/MemberMapper.java
+++ src/main/java/global/sesoc/library/mapper/MemberMapper.java
@@ -1,7 +1,10 @@
 package global.sesoc.library.mapper;
 
+import global.sesoc.library.vo.Members;
 
 public interface MemberMapper {
+	//회원 정보 저장
+	public int insertMember(Members member);
 	
 	
 }
