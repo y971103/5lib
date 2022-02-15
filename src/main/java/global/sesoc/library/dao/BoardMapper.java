720b4a3a23d8f2d12eb97e320ce260fc4f5820fe
KIDO <kkd9255@gmail.com>
2022-02-15 14:44:57 +0900

    Mapper.java

--- src/main/java/global/sesoc/library/dao/BoardMapper.java
+++ src/main/java/global/sesoc/library/dao/BoardMapper.java
@@ -1,35 +1,37 @@
-//package global.sesoc.library.dao;
-//
-//import java.util.ArrayList;
-//
-//import org.apache.ibatis.session.RowBounds;
-//
-//import global.sesoc.library.vo.QnA;
-//import global.sesoc.library.vo.Board_reply;
-//
-//public interface BoardMapper {
-//
-//	public int insertQnA(QnA qna);
-//
-//	public QnA getQnA(int qnAnum);
-//
-//	public void addHits(int qnAnum);
-//
-//	public int getTotal(String searchText);
-//
-//	public ArrayList<QnA> listQnA(String searchText, RowBounds rb);
-//
-//	public int deleteQnA(QnA qna);
-//
-//	public int updateQnA(QnA qna);
-//
-//	public int insertQnAReply(Board_reply qnareply);
-//
-//	public ArrayList<Board_reply> listQnAReply(int qnAnum);
-//
-//	public int deleteQnAReply(Board_reply qnareply);
-//
-//	public int updateQnAReply(Board_reply qnareply);
-//	
-//	
-//}
+package global.sesoc.library.dao;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
+
+import global.sesoc.library.vo.Board;
+import global.sesoc.library.vo.Reply;
+
+public interface BoardMapper {
+
+	public int insertBoard(Board board);
+
+	public Board getBoard(int boardnum);
+
+	public void addHits(int boardnum);
+
+	public int getTotal(String searchText);
+
+	public ArrayList<Notice> listNotice(String searchText, RowBounds rb);
+	
+	public ArrayList<QnA> listQnA(String searchText, RowBounds rb);
+
+	public int deleteBoard(Board board);
+
+	public int updateBoard(Board board);
+
+	public int insertReply(Reply reply);
+
+	public ArrayList<Reply> listReply(int boardnum);
+
+	public int deleteReply(Reply reply);
+
+	public int updateReply(Reply reply);
+	
+	
+}
