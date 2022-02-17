d3f08955a413dd5e576820565bf1393ff3a93fe9
KIDO <kkd9255@gmail.com>
2022-02-17 14:31:08 +0900

    //

--- src/main/java/global/sesoc/library/dao/BookMapper.java
+++ src/main/java/global/sesoc/library/dao/BookMapper.java
@@ -7,19 +7,17 @@ import global.sesoc.library.vo.Review;
 
 public interface BookMapper {
 	
-	//한줄평 작성
-	public int insertReview(Review review);
-	
-	//한줄평 삭제
-	public int deleteReview(Review review);
-	
-	//한줄평 수정
-	public int updateReview(Review review);
-	
-	//한줄평 목록 출력
-	public ArrayList<Review> listReview(int booknum);
+	/*
+	 * //한줄평 작성 public int insertReview(Review review);
+	 * 
+	 * //한줄평 삭제 public int deleteReview(Review review);
+	 * 
+	 * //한줄평 수정 public int updateReview(Review review);
+	 * 
+	 * //한줄평 목록 출력 public ArrayList<Review> listReview(int booknum);
+	 */
 
-	public ArrayList<Kakaobook> seletKakaobook();
+	public ArrayList<Kakaobook> selectKakaobook();
 	
 	
 }
