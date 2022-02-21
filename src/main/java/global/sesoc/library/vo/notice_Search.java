cdedab01a03d0535b751cbc4473ba3286cfdff41
CHOISEUNGHWAN <rmrdusdhkd100@gmail.com>
2022-02-21 11:32:38 +0900

    검색창 진행중

---
+++ src/main/java/global/sesoc/library/vo/notice_Search.java
@@ -0,0 +1,34 @@
+package global.sesoc.library.vo;
+
+public class notice_Search {
+	private String type = "1";
+	private String searchText = "";
+	
+	public notice_Search() {
+		
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getSearchText() {
+		return searchText;
+	}
+
+	public void setSearchText(String searchText) {
+		this.searchText = searchText;
+	}
+
+	@Override
+	public String toString() {
+		return "notice_Search [type=" + type + ", searchText=" + searchText + "]";
+	}
+	
+	
+
+}
