ab3ac9940d6f17f9701f1e2eabc53b9a38751bbc
KIMEUNJI <most33@naver.com>
2022-02-28 15:48:58 +0900

    Revert "comment dao 수정중"
    This reverts commit 283c5c0281336e3205b9c86a173698773621926c.

--- src/main/java/global/sesoc/library/dao/MypageDAO.java
+++ src/main/java/global/sesoc/library/dao/MypageDAO.java
@@ -2,15 +2,13 @@ package global.sesoc.library.dao;
 
 import java.util.ArrayList;
 
-import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import global.sesoc.library.vo.Board;
+
 import global.sesoc.library.vo.Comments;
 import global.sesoc.library.vo.Habit;
-import global.sesoc.library.vo.notice_Search;
 
 
 @Repository
@@ -19,16 +17,7 @@ public class MypageDAO {
 	@Autowired
 	SqlSession sqlSession;
 
-	//찜한 도서 목록 (게시판 형식) 
-	public ArrayList<shelf> shelflist () {
-		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
-		ArrayList<Board> boardlist = mapper.listBoard(notice_search, rb);
-		return shelflist;
-	}
-	
-	
-	
-	
+
 	// 내 서재에 책 코멘트 등록
 	public int insertComments(Comments comments) {
 		MypageMapper mapper = sqlSession.getMapper(MypageMapper.class);
