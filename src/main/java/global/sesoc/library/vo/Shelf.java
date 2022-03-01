6a445ee12ea1e37d44797b38f05f0c02b28e6b32
CHOISEUNGHWAN <rmrdusdhkd100@gmail.com>
2022-03-01 15:39:29 +0900

    ㅇㅇ

--- src/main/java/global/sesoc/library/vo/Shelf.java
+++ src/main/java/global/sesoc/library/vo/Shelf.java
@@ -2,17 +2,23 @@ package global.sesoc.library.vo;
 
  
 public class Shelf {
-	private int shelfnum;
+	
 	private String id;
 	private String isbn;
+	private String authors;
+	private String title;
+	private String thumbnail;
 	private String inputdate;
 	
 	
-	public Shelf(int shelfnum, String id, String isbn, String inputdate) {
+	public Shelf(String id, String isbn, String authors, String title, String thumbnail, String inputdate) {
 		super();
-		this.shelfnum = shelfnum;
+		
 		this.id = id;
 		this.isbn = isbn;
+		this.isbn = authors;
+		this.isbn = title;
+		this.isbn = thumbnail;
 		this.inputdate = inputdate;
 	}
 	
@@ -20,48 +26,97 @@ public class Shelf {
 		
 	}
 
-
-	public int getShelfnum() {
-		return shelfnum;
-	}
-
-
-	public void setShelfnum(int shelfnum) {
-		this.shelfnum = shelfnum;
-	}
-
-
+	/**
+	 * @return the id
+	 */
 	public String getId() {
 		return id;
 	}
 
-
+	/**
+	 * @param id the id to set
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
-
+	/**
+	 * @return the isbn
+	 */
 	public String getIsbn() {
 		return isbn;
 	}
 
-
+	/**
+	 * @param isbn the isbn to set
+	 */
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
-	
+
+	/**
+	 * @return the authors
+	 */
+	public String getAuthors() {
+		return authors;
+	}
+
+	/**
+	 * @param authors the authors to set
+	 */
+	public void setAuthors(String authors) {
+		this.authors = authors;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the thumbnail
+	 */
+	public String getThumbnail() {
+		return thumbnail;
+	}
+
+	/**
+	 * @param thumbnail the thumbnail to set
+	 */
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+
+	/**
+	 * @return the inputdate
+	 */
 	public String getInputdate() {
 		return inputdate;
 	}
 
-
+	/**
+	 * @param inputdate the inputdate to set
+	 */
 	public void setInputdate(String inputdate) {
 		this.inputdate = inputdate;
 	}
 
 	@Override
 	public String toString() {
-		return "Shelf [shelfnum=" + shelfnum + ", id=" + id + ", isbn=" + isbn + ", inputdate=" + inputdate + "]";
+		return "Shelf [id=" + id + ", isbn=" + isbn + ", authors=" + authors + ", title=" + title + ", thumbnail="
+				+ thumbnail + ", inputdate=" + inputdate + "]";
 	}
+
+
+	
 	
 }
