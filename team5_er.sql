eb7b3a637f32880d9acca75c7201e5d136c3f10b
KIM SOHWI <sohwe05@gmail.com>
2022-03-05 13:39:33 +0900

    DB수정

--- team5_er.sql
+++ team5_er.sql
@@ -1,28 +1,10 @@
-
-/* Drop Tables */
-
-DROP TABLE reply CASCADE CONSTRAINTS;
-DROP TABLE board CASCADE CONSTRAINTS;
-DROP TABLE comments CASCADE CONSTRAINTS;
-DROP TABLE essay CASCADE CONSTRAINTS;
-DROP TABLE genre CASCADE CONSTRAINTS;
-DROP TABLE review CASCADE CONSTRAINTS;
-DROP TABLE shelf CASCADE CONSTRAINTS;
-DROP TABLE kakaobook CASCADE CONSTRAINTS;
-DROP TABLE genre_info CASCADE CONSTRAINTS;
-DROP TABLE habit CASCADE CONSTRAINTS;
-DROP TABLE members CASCADE CONSTRAINTS;
-
-
-
-
 /* Create Tables */
 
 CREATE TABLE board
 (
 	-- 게시글 번호
 	boardnum number NOT NULL,
-	-- 게시글 타입
+	-- 게시글 타입 (공지 / 문의)
 	board_type number,
 	-- 공지 제목
 	title varchar2(200),
@@ -32,7 +14,7 @@ CREATE TABLE board
 	content varchar2(2000),
 	-- 공지 작성 일자
 	inputdate date DEFAULT SYSDATE,
-	-- 회원 아이디
+	-- 글 작성한 회원 아이디
 	id varchar2(20) NOT NULL,
 	PRIMARY KEY (boardnum)
 );
@@ -66,26 +48,7 @@ CREATE TABLE essay
 	hits number,
 	-- 에세이 작성 일자
 	inputdate date DEFAULT SYSDATE,
-	-- 책 번호
-	isbn varchar2(200) NOT NULL,
 	PRIMARY KEY (essaynum)
-);
-
-
-CREATE TABLE genre
-(
-	-- 회원 아이디
-	id varchar2(20) NOT NULL,
-	genre_num number(2) NOT NULL,
-	UNIQUE (id, genre_num)
-);
-
-
-CREATE TABLE genre_info
-(
-	genre_num number(2) NOT NULL,
-	genre_name varchar2(100),
-	PRIMARY KEY (genre_num)
 );
 
 
@@ -95,10 +58,12 @@ CREATE TABLE habit
 	id varchar2(20) NOT NULL,
 	-- 독서 시간
 	time number,
-	-- 읽은 권수
-	amount number,
-	-- 타임과 비교할 현재 시각
-	inputdate date DEFAULT SYSDATE
+	-- 뷰어를 연 시각
+	inputdate date DEFAULT SYSDATE,
+	-- 읽은 도서
+	isbn varchar2(200) NOT NULL,
+	-- 뷰어 연 횟수(sum으로 계산)
+	hits number DEFAULT 0 NOT NULL
 );
 
 
@@ -110,18 +75,20 @@ CREATE TABLE kakaobook
 	title varchar2(200),
 	-- 작가
 	author varchar2(20),
+	-- 책 소개
+	contents varchar2(2000),
+	-- 출판 일자
+	datetime varchar2(100),
 	-- 출판사
 	publisher varchar2(50),
-	-- 출판 일자
-	datetime date,
-	-- 장르
-	genre_type number,
 	-- 책 사진
-	thumbnail number,
-	content varchar2(2000),
-	-- 책 파일
-	bookfile number,
-	genre_num number(2) NOT NULL,
+	thumbnail varchar2(200),
+	-- 책번호
+	booknum number,
+	-- 장르
+	genre varchar2(200),
+	-- 베스트셀러용 책 조회수
+	hits number DEFAULT 0 NOT NULL,
 	PRIMARY KEY (isbn)
 );
 
@@ -135,12 +102,13 @@ CREATE TABLE members
 	-- 회원 이름
 	name varchar2(20),
 	-- 회원 전화번호
-	phone number,
+	phone varchar2(20),
 	-- 회원 이메일
 	email varchar2(20),
 	-- 회원 구독 정보
-	sub number,
-	genre_num number,
+	sub varchar2(20),
+	-- 선호 장르
+	genre varchar2(50),
 	PRIMARY KEY (id)
 );
 
@@ -149,9 +117,9 @@ CREATE TABLE reply
 (
 	-- 댓글 번호
 	replynum number NOT NULL,
-	-- reply 내용
+	-- 댓글 내용
 	content varchar2(200),
-	-- reply 회원 아이디
+	-- 댓글 작성 회원 아이디
 	id varchar2(20) NOT NULL,
 	-- 게시글 번호
 	boardnum number NOT NULL,
@@ -164,11 +132,9 @@ CREATE TABLE review
 	-- 한줄평 번호 (라이브러리)
 	reviewnum number NOT NULL,
 	-- 한줄평 작성 날짜
-	inputdate date,
+	inputdate date default sysdate,
 	-- 한줄평 내용
 	content varchar2(200),
-	-- 한줄평 좋아요
-	likecnt number,
 	-- 회원 아이디
 	id varchar2(20) NOT NULL,
 	-- 책 번호
@@ -183,8 +149,16 @@ CREATE TABLE shelf
 	id varchar2(20) NOT NULL,
 	-- 책 번호
 	isbn varchar2(200) NOT NULL,
-	-- 내 서재에 등록된 책 수
-	shelfnum number NOT NULL
+	-- 내서재에 찜한 날짜
+	inputdate date DEFAULT SYSDATE,
+	-- 찜한 도서의 작가
+	authors varchar2(20),
+	-- 찜한 도서의 제목
+	title varchar2(200),
+	-- 찜한 도서의 표지
+	thumbnail varchar2(200)
+	-- 복합키
+    	CONSTRAINT shelf_PK PRIMARY KEY(id, isbn)
 );
 
 
@@ -197,25 +171,7 @@ ALTER TABLE reply
 ;
 
 
-ALTER TABLE genre
-	ADD FOREIGN KEY (genre_num)
-	REFERENCES genre_info (genre_num)
-;
-
-
-ALTER TABLE kakaobook
-	ADD FOREIGN KEY (genre_num)
-	REFERENCES genre_info (genre_num)
-;
-
-
 ALTER TABLE comments
-	ADD FOREIGN KEY (isbn)
-	REFERENCES kakaobook (isbn)
-;
-
-
-ALTER TABLE essay
 	ADD FOREIGN KEY (isbn)
 	REFERENCES kakaobook (isbn)
 ;
@@ -251,12 +207,6 @@ ALTER TABLE essay
 ;
 
 
-ALTER TABLE genre
-	ADD FOREIGN KEY (id)
-	REFERENCES members (id)
-;
-
-
 ALTER TABLE habit
 	ADD FOREIGN KEY (id)
 	REFERENCES members (id)
@@ -285,12 +235,12 @@ ALTER TABLE shelf
 /* Comments */
 
 COMMENT ON COLUMN board.boardnum IS '게시글 번호';
-COMMENT ON COLUMN board.board_type IS '게시글 타입';
+COMMENT ON COLUMN board.board_type IS '게시글 타입 (공지 / 문의)';
 COMMENT ON COLUMN board.title IS '공지 제목';
 COMMENT ON COLUMN board.hits IS '조회수';
 COMMENT ON COLUMN board.content IS '공지 내용';
 COMMENT ON COLUMN board.inputdate IS '공지 작성 일자';
-COMMENT ON COLUMN board.id IS '회원 아이디';
+COMMENT ON COLUMN board.id IS '글 작성한 회원 아이디';
 COMMENT ON COLUMN comments.commentnum IS '나만 보는 리뷰 번호 (줄글)';
 COMMENT ON COLUMN comments.content IS '나만 보는 리뷰 내용';
 COMMENT ON COLUMN comments.id IS '회원 아이디';
@@ -301,26 +251,26 @@ COMMENT ON COLUMN essay.content IS '에세이 내용';
 COMMENT ON COLUMN essay.id IS '회원 아이디';
 COMMENT ON COLUMN essay.hits IS '조회수';
 COMMENT ON COLUMN essay.inputdate IS '에세이 작성 일자';
-COMMENT ON COLUMN essay.isbn IS '책 번호';
-COMMENT ON COLUMN genre.id IS '회원 아이디';
 COMMENT ON COLUMN habit.id IS '회원 아이디';
 COMMENT ON COLUMN habit.time IS '독서 시간';
-COMMENT ON COLUMN habit.amount IS '읽은 권수';
-COMMENT ON COLUMN habit.inputdate IS '타임과 비교할 현재 시각';
+COMMENT ON COLUMN habit.inputdate IS '뷰어를 연 시각';
+COMMENT ON COLUMN habit.isbn IS '읽은 도서';
+COMMENT ON COLUMN habit.hits IS '뷰어 연 횟수(sum으로 계산)';
 COMMENT ON COLUMN kakaobook.isbn IS '책 번호';
 COMMENT ON COLUMN kakaobook.title IS '책 제목';
 COMMENT ON COLUMN kakaobook.author IS '작가';
 COMMENT ON COLUMN kakaobook.publisher IS '출판사';
 COMMENT ON COLUMN kakaobook.datetime IS '출판 일자';
-COMMENT ON COLUMN kakaobook.genre_type IS '장르';
+COMMENT ON COLUMN kakaobook.genre IS '장르';
 COMMENT ON COLUMN kakaobook.thumbnail IS '책 사진';
-COMMENT ON COLUMN kakaobook.bookfile IS '책 파일';
+COMMENT ON COLUMN kakaobook.hits IS '베스트셀러용 책 조회수';
 COMMENT ON COLUMN members.id IS '회원 아이디';
 COMMENT ON COLUMN members.password IS '회원 비밀번호';
 COMMENT ON COLUMN members.name IS '회원 이름';
 COMMENT ON COLUMN members.phone IS '회원 전화번호';
 COMMENT ON COLUMN members.email IS '회원 이메일';
 COMMENT ON COLUMN members.sub IS '회원 구독 정보';
+COMMENT ON COLUMN members.genre IS '선호 장르';
 COMMENT ON COLUMN reply.replynum IS '댓글 번호';
 COMMENT ON COLUMN reply.content IS 'reply 내용';
 COMMENT ON COLUMN reply.id IS 'reply 회원 아이디';
@@ -328,12 +278,27 @@ COMMENT ON COLUMN reply.boardnum IS '게시글 번호';
 COMMENT ON COLUMN review.reviewnum IS '한줄평 번호 (라이브러리)';
 COMMENT ON COLUMN review.inputdate IS '한줄평 작성 날짜';
 COMMENT ON COLUMN review.content IS '한줄평 내용';
-COMMENT ON COLUMN review.likecnt IS '한줄평 좋아요';
 COMMENT ON COLUMN review.id IS '회원 아이디';
 COMMENT ON COLUMN review.isbn IS '책 번호';
 COMMENT ON COLUMN shelf.id IS '회원 아이디';
 COMMENT ON COLUMN shelf.isbn IS '책 번호';
-COMMENT ON COLUMN shelf.shelfnum IS '내 서재에 등록된 책 수';
+COMMENT ON COLUMN shelf.inputdate IS '내서재에 찜한 날짜';
+COMMENT ON COLUMN shelf.authors IS '찜한 도서의 작가';
+COMMENT ON COLUMN shelf.title IS '찜한 도서의 제목';
+COMMENT ON COLUMN shelf.thumbnail IS '찜한 도서의 표지';
 
 
+-- 게시판 글 번호에 사용할 시퀀스
+create sequence board_seq;
 
+-- 게시글 댓글 번호에 사용할 시퀀스 
+create sequence reply_seq;
+
+-- 에세이 글 번호에 사용할 시퀀스
+create sequence essay_seq;
+
+-- 한줄평 댓글 번호에 사용할 시퀀스
+create sequence review_seq;    
+
+--comments에 사용할 시퀀스
+create sequence comments_seq;
